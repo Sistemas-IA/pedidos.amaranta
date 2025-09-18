6ee97f4ae73a4cec241b7fe8929b5ecb6e5b483f
Sistemas-IA <ihermanowyc@gmail.com>
2025-09-17 22:09:58 -0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,11 +1,12 @@
 // config/config.js
 const cfg = {
   endpoints: {
-    login: "https://script.google.com/macros/s/REEMPLAZAR/exec?action=login",
-    viandas: "https://script.google.com/macros/s/REEMPLAZAR/exec?action=viandas",
-    pedido: "https://script.google.com/macros/s/REEMPLAZAR/exec?action=pedido"
+    // Usá SIEMPRE esta base con ?action=
+    login:  "https://script.google.com/macros/s/AKfycbxRcJBwiduLWnfRaGJl0UaTH0bIk0bmi2KLtgmEG20jG6j08Srrg5mluFpTylik1THdfA/exec?action=login",
+    viandas:"https://script.google.com/macros/s/AKfycbxRcJBwiduLWnfRaGJl0UaTH0bIk0bmi2KLtgmEG20jG6j08Srrg5mluFpTylik1THdfA/exec?action=viandas",
+    pedido: "https://script.google.com/macros/s/AKfycbxRcJBwiduLWnfRaGJl0UaTH0bIk0bmi2KLtgmEG20jG6j08Srrg5mluFpTylik1THdfA/exec?action=pedido"
   },
   origin: "https://pedidos.amaranta.ar",
-  recaptchaSiteKey: "" // opcional
+  recaptchaSiteKey: "" // si vas a usar reCAPTCHA, poné tu site key acá
 };
 export default cfg;
