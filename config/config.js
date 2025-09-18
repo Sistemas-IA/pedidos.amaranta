9a358c7108b26969f036b35aea4b93d42ea07cdc
Sistemas-IA <ihermanowyc@gmail.com>
2025-09-17 21:50:37 -0300

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,11 @@
+// config/config.js
+const cfg = {
+  endpoints: {
+    login: "https://script.google.com/macros/s/REEMPLAZAR/exec?action=login",
+    viandas: "https://script.google.com/macros/s/REEMPLAZAR/exec?action=viandas",
+    pedido: "https://script.google.com/macros/s/REEMPLAZAR/exec?action=pedido"
+  },
+  origin: "https://pedidos.amaranta.ar",
+  recaptchaSiteKey: "" // opcional
+};
+export default cfg;
